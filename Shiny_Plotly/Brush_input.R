2d91f66899d1196cf78783b9c8d340b11e58708b
shanl33 <shanlee@Shans-MacBook-Pro.local>
2017-03-28 10:25:10 +1300

    Rename files Technique_tool/output

--- Shiny_Plotly/Brush_input.R
+++ Shiny_Plotly/Brush_input.R
@@ -4,7 +4,7 @@ library(shiny)
 library(dplyr)
 load("~/Desktop/Project/slee205_Honours/Datasets/AQ.RData")
 
-AQ$DayofNetDelay <- as.integer(AQ$DayofNetDelay)
+AQ$NetDelay <- AQ$ArrDelay-AQ$DepDelay
 AQ$DayOfWeek <- as.integer(AQ$DayOfWeek)
 
 ui <- fluidPage(
@@ -15,7 +15,7 @@ ui <- fluidPage(
 server <- function(input, output) {
   output$scatterplot <- renderPlotly({
     plot_ly(data=AQ, x=AQ$DayofMonth, y=AQ$NetDelay, 
-            color=AQ$DayOfWeek, source = "subset") %>%
+            color=AQ$DayOfWeek, source = "subset", mode = "markers", mode = "scatter") %>%
       layout(xaxis=list(title="Day of Month"), yaxis=list(title="Net Delay"))
   })
   
@@ -31,11 +31,12 @@ server <- function(input, output) {
     plot.subset <- rbind(wkend, wkday)
     # Summarise
     plot.sum <- plot.subset %>%
-      group_by(DayOfWeek, DayofMonth, NetDelay) %>%
+      group_by(DayOfWeek) %>%
       summarize(Mean = mean(NetDelay))
     # Assign to parent frame
     plot.sum <<- plot.sum
     # Plot with mean net delay.  Not sure why bars are up and down.
+    # Colour of bars could not be set to a "new" variable, not previously used.
     plot_ly(plot.sum, x = plot.sum$DayOfWeek, y=plot.sum$Mean, 
             type="bar", source = "select", color = plot.sum$DayOfWeek) %>%
       layout(xaxis=list(title="Day of Week"), yaxis=list(title="Mean net delay (mins)"))
