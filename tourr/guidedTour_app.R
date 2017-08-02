ee5a4c64bd9782e6c5d0af03c67710ff391a5e2b
shanl33 <shanlee@Shans-MacBook-Pro.local>
2017-08-03 11:15:42 +1200

    Use with NCEA data?

--- tourr/guidedTour_app.R
+++ tourr/guidedTour_app.R
@@ -241,3 +241,45 @@ pp2D_xtalk(lcrabs, index = "holes")
 data("mtcars")
 mtcars[, c(2,8,9,10,11)] <- lapply(mtcars[, c(2,8,9,10,11)], factor)
 guidedTour_app(mtcars, factors = 4)
+
+# What happens with NCEA data?
+# NCEA Data setup --------------------------------------------------------------
+library(dplyr)
+nzqa2016 <- read.csv("http://www.nzqa.govt.nz/assets/Studying-in-NZ/Secondary-school-and-NCEA/stats-reports/2016/Qualification-Statistics-School-2016-29032017.csv")
+# Drop vars that will not be used (eg. cumulative achievement)
+nzqa <- nzqa2016[,-c(1,8,10)]
+names(nzqa) <- c("Decile", "Region", "School", "Year", "Qualification", "Achieve_participate", "Achieve_roll", "Small_sch")
+levels(nzqa$Qualification) <- c("L1", "L2", "L3", "UE")
+# Subset to use only Year 11 with Level 1, etc
+nzqa <- nzqa %>% filter(((Qualification=="L1")&(Year==11))|((Year==12) & (Qualification=="L2"))|
+                          ((Year==13) & (Qualification=="L3"))|((Year==13) & (Qualification=="UE")))
+# Reshape so that one row = one school
+# Current.Achievement.Rate.Participation kept for analysis only
+achieved <- nzqa %>% 
+  spread(Qualification, Achieve_participate, fill=0) %>%
+  group_by(School) %>%
+  summarise_at(c("L1", "L2", "L3", "UE"), sum) %>%
+  inner_join(nzqa[, c(1, 2, 3, 8)]) %>% #Add Decile and Region and Small_sch variables
+  distinct() %>% #One row per school
+  filter(!((L1==0)&(L2==0)&(L3==0))) #Remove schools with 0% achievement rate for all levels
+# Function to replace 0% with NA
+zeros <- function(col) {
+  replace(col, col==0, NA)
+}
+achieved$L1 <- zeros(achieved$L1)
+achieved$L2 <- zeros(achieved$L2)
+achieved$L3 <- zeros(achieved$L3)
+achieved$UE <- zeros(achieved$UE)
+# Remove schools with 'small cohort' warning (obscures pattern in non-small cohorts).
+achieved <- achieved[achieved$Small_sch=="",] #437 school left
+achieved <- achieved[, -8]
+# 'achieved' contains schools with ONE or more % achievement rate (by participation)
+# NA's used otherwise
+# Small cohort schools removed
+# Remove obs with any NA values 
+ach_narm <- achieved[complete.cases(achieved),] #407 schools left
+ach_narm$Decile <- as.factor(ach_narm$Decile)
+rownames(ach_narm) <- ach_narm$School
+
+# Run 
+guidedTour_app(ach_narm, index = "holes")
