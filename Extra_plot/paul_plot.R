4dc2a291c6850b19b7b4d2f54d2c757b1e56b320
shanl33 <shanlee@wifi-172-23-53-188.uoa-wifi.auckland.ac.nz>
2017-11-22 13:48:20 +1300

    labels

--- Extra_plot/paul_plot.R
+++ Extra_plot/paul_plot.R
@@ -2,17 +2,17 @@ library(ggplot2)
 
 bus6215 <- read.csv("Files/bus-6215-2017-04-01.csv", header=FALSE)
 colnames(bus6215) <- c("trip", "route", "vehicle", "departure", "arrival", "stop_sequence", "stop_id", "timestamp")
-head(bus6215)
-summary(bus6215)
-levels(bus6215$trip)
 bus6215$stop <- as.factor(bus6215$stop_id)
-levels(bus6215$stop)
 
 # Distance from city
 distance <- data.frame(stop = c("7018", "1464", "7060", "7062", "7164", "7168",
                                 "8502", "8512", "8516", "8524", "8532", "2006"), 
                        dist = c(0, 0.65, 1.65, 1.95, 2.55, 2.8, 3.5, 5.3, 5.75, 
                                 6.95, 8.15, 21.65))
+bus_stops <- c("Customs St East (Mercure Hotel)", "64 Hobson St", "380 Queen St", "490 Queen St",
+               "Symonds St overbridge", "Opposite 157 Symonds St", "Mt Eden Rd by train station", 
+               "Opposite 459 Mt Eden Rd", "588 Mt Eden Rd", "822 Mt Eden Rd", "Three Kings School",
+               "AKL Airport")
 
 # Subset route 30002 (City to Airport via Mt Eden)
 airport <- bus6215[grep("^30002-*", bus6215$route), ]
@@ -24,9 +24,14 @@ airport_dist <- merge(uniqueairport, distance)
 # Plot (I need to add more details for y-axis, labels etc..)
 # Colour just for testing
 ggplot(airport_dist, aes(x=timestamp, y=dist)) +
-  geom_point() +
-  geom_line(aes(group=trip, colour=trip)) +
-  labs(y="Distance from City")
+  geom_line(aes(group=trip)) +
+  theme_bw() +
+  scale_y_continuous(name=NULL, breaks=distance$dist, labels=bus_stops, expand=c(0, 0)) +
+  #scale_x_continuous(name=NULL, breaks=seq(0, 24, 1), minor_breaks=seq(0, 24, 1/6), expand=c(0, 0)) +
+  theme(panel.grid.minor=element_blank(),
+        panel.grid.major.y=element_line(colour="grey30"),
+        panel.border=element_blank(),
+        axis.ticks=element_blank())
 
 # Ideas/To do's:
 ## 1. Possible format for x's: Time in hours from midnight
